ab6cd2693073ac758205446bbf289e136c1f8e19
AlejandroDVDSM <bysalexx@gmail.com>
2025-02-08 18:12:04 +0000

    Add key to pause audio

---
+++ audio.js
@@ -0,0 +1,17 @@
+﻿// load some sound
+const audioElement = document.querySelector("audio");
+
+// register event listeners to store key actions and keycodes inside of GLOBALS, so that they can be accessed by sprites that need them.
+
+window.addEventListener("keydown", (e) => {
+    if (e.defaultPrevented) {
+        return; // Do nothing if event already handled
+    }
+
+    if (e.code == "KeyM") {
+        if (audio.playState == "paused")
+            audio.play();
+        else if (audio.playState == "playing")
+            audio.pause();
+    }
+});
