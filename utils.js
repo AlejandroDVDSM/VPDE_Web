d7d40f73e5cb62a5e5a3a228bf5c215a17b610d5
AlejandroDVDSM <bysalexx@gmail.com>
2025-02-08 18:33:24 +0000

    Add fullscreen mode when pressing F key

---
+++ utils.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("keydown", (e) => {
+    if (e.code === "KeyF")
+        setFullscreen();
+});
+
+function setFullscreen() {
+    if (!document.fullscreenElement)
+        document.documentElement.requestFullscreen();
+    else if (document.exitFullscreen)
+        document.exitFullscreen();
+}
